40b330ce43ff5e2371f1dc685a03f2f31652593d
J. Henneberg <6606609+JHenneberg@users.noreply.github.com>
2018-06-08 14:35:05 +0200

    added complete set of test_asserts
    all asserts are tested, all data types, not passed and passed

--- src/test/test_assert.F90
+++ src/test/test_assert.F90
@@ -3,19 +3,63 @@ program test_assert
   use unit_test
 
   call test_case_init()
-
   call test_case_create('Test 1')
+  
 
+  ! assert_approximate 
   call assert_approximate(1.0, 2.0)
+  call assert_approximate(1.0, 1.0)
+  
+  call assert_approximate(1.0, 1.10, 0.1)
+  call assert_approximate(1.0, 1.01, 0.1)
+  
+  call assert_approximate([1.0, 1.0], [1.0, 1.10], 0.1)
+  call assert_approximate([1.0, 1.0], [1.0, 1.01], 0.1)
 
+  ! assert_false/_true
   call assert_false(.true.)
-
+  call assert_false(.false.)
+  
+  call assert_true(.false.)
   call assert_true(.true.)
 
+  ! assert_equal
+  call assert_equal('abc', 'abcd')
   call assert_equal('abc', 'abc')
+  
+  call assert_equal(1, 2)
+  call assert_equal(3, 3)
+  
+  call assert_equal(1.0, 2.0)
+  call assert_equal(3.0, 3.0)
+  
+  call assert_equal(1.0D0, 2.0D0)
+  call assert_equal(3.0D0, 3.0D0)
+  
+  call assert_equal(['abc', 'abc'], ['abc', 'abd'])
+  call assert_equal(['abc', 'abc'], ['abc', 'abc'])
+  
+  call assert_equal([1, 1, 1, 1], [1, 2, 1, 1])
+  call assert_equal([1, 1, 1, 1], [1, 1, 1, 1])
+  
+  call assert_equal([1.0, 1.0], [1.0, 2.0])
+  call assert_equal([1.0, 1.0], [1.0, 1.0])
+  
+  call assert_equal([1.0D0, 1.0D0], [1.0D0, 2.0D0])
+  call assert_equal([1.0D0, 1.0D0], [1.0D0, 1.0D0])
+  
+  ! assert_great_than
+  call assert_great_than(1, 1)
+  call assert_great_than(2, 1)
+  
+  call assert_great_than(1.0, 1.0)
+  call assert_great_than(2.0, 1.0)
+  
+  call assert_great_than(1.0D0, 1.0D0)
+  call assert_great_than(2.0D0, 1.0D0)
 
+  
   call test_case_report('Test 1')
-
   call test_case_final()
 
 end program test_assert
