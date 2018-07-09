42068365bc82c9ccda3a607d0ba726de4bad2330
J. Henneberg <6606609+JHenneberg@users.noreply.github.com>
2018-07-09 10:53:42 +0200

    added new test routines for assert_great_than
    INT8, INT16, INT32, INT64 support;
    vector and array support for all data_types

--- src/test/test_assert.F90
+++ src/test/test_assert.F90
@@ -1,4 +1,5 @@
 program test_assert
+  ! program to test assert routines
 
   use unit_test
   
@@ -7,119 +8,188 @@ program test_assert
   type(test_suite_type) :: test_suite_equal
   type(test_suite_type) :: test_suite_great_than
   
-  ! assert_approximate  
+  ! test assert_approximate routines
   test_suite_approximate%name = 'Approximate'
   
-  ! -> real4
-  call test_case_create('real4', test_suite_approximate)
+  ! -> real
+  call test_case_create('real', test_suite_approximate)
   call assert_approximate(1.0, 2.0, suite=test_suite_approximate)
   call assert_approximate(1.0, 1.0, suite=test_suite_approximate)
   call assert_approximate(1.0, 1.10, eps=0.1, suite=test_suite_approximate)
   call assert_approximate(1.0, 1.01, eps=0.1, suite=test_suite_approximate)
-  call assert_approximate([1.0, 1.0], [1.0, 1.10], suite=test_suite_approximate)
-  call assert_approximate([1.0, 1.0], [1.0, 1.00000000001], suite=test_suite_approximate)
-  call assert_approximate([1.0, 1.0], [1.0, 1.10], eps=0.1, suite=test_suite_approximate)
-  call assert_approximate([1.0, 1.0], [1.0, 1.01], eps=0.1, suite=test_suite_approximate)
-  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.1, 1.0, 1.0], [2, 2]), suite=test_suite_approximate)
-  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.00000000001, 1.0, 1.0], [2, 2]), suite=test_suite_approximate)
-  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.1, 1.0, 1.0], [2, 2]), eps=0.1, suite=test_suite_approximate)
-  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.01, 1.0, 1.0], [2, 2]), eps=0.1, suite=test_suite_approximate)
-  
-  ! -> real8
-  call test_case_create('real8', test_suite_approximate)
   call assert_approximate(1.0D0, 1.1D0, suite=test_suite_approximate)
   call assert_approximate(1.0D0, 1.00000000001D0, suite=test_suite_approximate)
   call assert_approximate(1.0D0, 1.1D0, eps=0.1D0, suite=test_suite_approximate)
   call assert_approximate(1.0D0, 1.00000000001D0, eps=0.1D0, suite=test_suite_approximate)
+  
+  call assert_approximate([1.0, 1.0], [1.0, 1.10], suite=test_suite_approximate)
+  call assert_approximate([1.0, 1.0], [1.0, 1.00000000001], suite=test_suite_approximate)
+  call assert_approximate([1.0, 1.0], [1.0, 1.10], eps=0.1, suite=test_suite_approximate)
+  call assert_approximate([1.0, 1.0], [1.0, 1.01], eps=0.1, suite=test_suite_approximate)
   call assert_approximate([1.0D0, 1.0D0], [1.0D0, 1.1D0], suite=test_suite_approximate)
   call assert_approximate([1.0D0, 1.0D0], [1.0D0, 1.00000000001D0], suite=test_suite_approximate)
   call assert_approximate([1.0D0, 1.0D0], [1.0D0, 1.1D0], eps=0.1D0, suite=test_suite_approximate)
   call assert_approximate([1.0D0, 1.0D0], [1.0D0, 1.01D0], eps=0.1D0, suite=test_suite_approximate)
+  
+  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.1, 1.0, 1.0], [2, 2]), suite=test_suite_approximate)
+  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.00000000001, 1.0, 1.0], [2, 2]), suite=test_suite_approximate)
+  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.1, 1.0, 1.0], [2, 2]), eps=0.1, suite=test_suite_approximate)
+  call assert_approximate(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.01, 1.0, 1.0], [2, 2]), eps=0.1, suite=test_suite_approximate)
   call assert_approximate(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.1D0, 1.0D0, 1.0D0], [2, 2]), suite=test_suite_approximate)
   call assert_approximate(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.000000000001D0, 1.0D0, 1.0D0], [2, 2]), suite=test_suite_approximate)
   call assert_approximate(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.1D0, 1.0D0, 1.0D0], [2, 2]), eps=0.1D0, suite=test_suite_approximate)
   call assert_approximate(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.01D0, 1.0D0, 1.0D0], [2, 2]), eps=0.1D0, suite=test_suite_approximate)
   
-  ! -> report
   call test_suite_report(test_suite_approximate)
   call test_case_final(test_suite_approximate)
   
-  ! assert_false/_true
+  ! test assert_false/_true routines
   test_suite_boolean%name = 'Boolean'
   
   ! -> false
   call test_case_create('false', test_suite_boolean)
-  call assert_false(.true., suite=test_suite_boolean)
-  call assert_false(.false., suite=test_suite_boolean)
+  call assert_false(.true., __FILE__, __LINE__, test_suite_boolean)
+  call assert_false(.false., __FILE__, __LINE__, test_suite_boolean)
   
   ! -> true
   call test_case_create('true', test_suite_boolean)
-  call assert_true(.false., suite=test_suite_boolean)
-  call assert_true(.true., suite=test_suite_boolean)
+  call assert_true(.false., __FILE__, __LINE__, test_suite_boolean)
+  call assert_true(.true., __FILE__, __LINE__, test_suite_boolean)
   
-  ! report
   call test_suite_report(test_suite_boolean)
   call test_case_final(test_suite_boolean)
   
-  ! assert_equal  
+  ! test assert_equal routines
   test_suite_equal%name = 'Equal'
   
   ! -> string
   call test_case_create('string', test_suite_equal)
-  call assert_equal('abc', 'abcd', suite=test_suite_equal)
-  call assert_equal('abc', 'abc', suite=test_suite_equal)
-  call assert_equal(['abc', 'abc'], ['abc', 'abd'], suite=test_suite_equal)
-  call assert_equal(['abc', 'abc'], ['abc', 'abc'], suite=test_suite_equal)
+  call assert_equal('abc', 'abcd', __FILE__, __LINE__, test_suite_equal)
+  call assert_equal('abc', 'abc', __FILE__, __LINE__, test_suite_equal)
   
-  ! -> integer
+  call assert_equal(['abc', 'abc'], ['abc', 'abd'], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(['abc', 'abc'], ['abc', 'abc'], __FILE__, __LINE__, test_suite_equal)
+  
+  call assert_equal(reshape(['abc', 'abc', 'abc', 'abc'], [2, 2]), reshape(['abc', 'abd', 'abc', 'abd'], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape(['abc', 'abc', 'abc', 'abc'], [2, 2]), reshape(['abc', 'abc', 'abc', 'abc'], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  
+  ! -> integer (int8->kind=1, int16->kind=2, int32->kind=4, int64->kind=8)
   call test_case_create('integer', test_suite_equal)
-  call assert_equal(1, 2, suite=test_suite_equal)
-  call assert_equal(3, 3, suite=test_suite_equal)
-  call assert_equal([1, 1, 1, 1], [1, 2, 1, 1], suite=test_suite_equal)
-  call assert_equal([1, 1, 1, 1], [1, 1, 1, 1], suite=test_suite_equal)
-  call assert_equal(reshape([1, 1, 1, 1], [2, 2]), reshape([1, 2, 1, 1], [2, 2]), suite=test_suite_equal)
-  call assert_equal(reshape([1, 1, 1, 1], [2, 2]), reshape([1, 1, 1, 1], [2, 2]), suite=test_suite_equal)
+  call assert_equal(int(1, 1), int(2, 1), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(3, 1), int(3, 1), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(1, 2), int(2, 2), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(3, 2), int(3, 2), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(1, 4), int(2, 4), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(3, 4), int(3, 4), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(1, 8), int(2, 8), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(int(3, 8), int(3, 8), __FILE__, __LINE__, test_suite_equal)
   
-  ! -> real4
-  call test_case_create('real4', test_suite_equal)
-  call assert_equal(1.0, 2.0, suite=test_suite_equal)
-  call assert_equal(3.0, 3.0, suite=test_suite_equal)
-  call assert_equal([1.0, 1.0], [1.0, 2.0], suite=test_suite_equal)
-  call assert_equal([1.0, 1.0], [1.0, 1.0], suite=test_suite_equal)
-  call assert_equal(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 2.0, 1.0, 1.0], [2, 2]), suite=test_suite_equal)
-  call assert_equal(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), suite=test_suite_equal)
+  call assert_equal([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [int(1, 1), int(2, 1), int(1, 1), int(1, 1)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [int(1, 1), int(1, 1), int(1, 1), int(1, 1)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [int(1, 2), int(2, 2), int(1, 2), int(1, 2)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [int(1, 2), int(1, 2), int(1, 2), int(1, 2)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [int(1, 4), int(2, 4), int(1, 4), int(1, 4)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [int(1, 4), int(1, 4), int(1, 4), int(1, 4)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [int(1, 8), int(2, 8), int(1, 8), int(1, 8)], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [int(1, 8), int(1, 8), int(1, 8), int(1, 8)], __FILE__, __LINE__, test_suite_equal)
   
-  ! -> real8
-  call test_case_create('real8', test_suite_equal)
-  call assert_equal(1.0D0, 2.0D0, suite=test_suite_equal)
-  call assert_equal(3.0D0, 3.0D0, suite=test_suite_equal)
-  call assert_equal([1.0D0, 1.0D0], [1.0D0, 2.0D0], suite=test_suite_equal)
-  call assert_equal([1.0D0, 1.0D0], [1.0D0, 1.0D0], suite=test_suite_equal)
-  call assert_equal(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 2.0D0, 1.0D0, 1.0D0], [2, 2]), suite=test_suite_equal)
-  call assert_equal(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), suite=test_suite_equal)
+  call assert_equal(reshape([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [2, 2]), &
+      & reshape([int(1, 1), int(2, 1), int(1, 1), int(1, 1)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [2, 2]), &
+      & reshape([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [2, 2]), &
+      & reshape([int(1, 2), int(2, 2), int(1, 2), int(1, 2)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [2, 2]), &
+      & reshape([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [2, 2]), &
+      & reshape([int(1, 4), int(2, 4), int(1, 4), int(1, 4)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [2, 2]), &
+      & reshape([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [2, 2]), &
+      & reshape([int(1, 8), int(2, 8), int(1, 8), int(1, 8)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [2, 2]), &
+      & reshape([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  
+  ! -> real (real4, real8)
+  call test_case_create('real', test_suite_equal)
+  call assert_equal(1.0, 2.0, __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(3.0, 3.0, __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(1.0D0, 2.0D0, __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(3.0D0, 3.0D0, __FILE__, __LINE__, test_suite_equal)
+  
+  call assert_equal([1.0, 1.0], [1.0, 2.0], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([1.0, 1.0], [1.0, 1.0], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([1.0D0, 1.0D0], [1.0D0, 2.0D0], __FILE__, __LINE__, test_suite_equal)
+  call assert_equal([1.0D0, 1.0D0], [1.0D0, 1.0D0], __FILE__, __LINE__, test_suite_equal)
+  
+  call assert_equal(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 2.0, 1.0, 1.0], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 2.0D0, 1.0D0, 1.0D0], [2, 2]), __FILE__, __LINE__, test_suite_equal)
+  call assert_equal(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), __FILE__, __LINE__, test_suite_equal)
   
   call test_suite_report(test_suite_equal)
   call test_case_final(test_suite_equal)
 
-  ! assert_great_than
+  ! test assert_great_than routines
   test_suite_great_than%name = 'great_then'
   
-  ! -> integer
+  ! -> integer (int8->kind=1, int16->kind=2, int32->kind=4, int64->kind=8)
   call test_case_create('integer', test_suite_great_than)
-  call assert_great_than(1, 1, suite=test_suite_great_than)
-  call assert_great_than(2, 1, suite=test_suite_great_than)
+  call assert_great_than(int(1, 1), int(1, 1), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(2, 1), int(1, 1), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(1, 2), int(1, 2), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(2, 2), int(1, 2), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(1, 4), int(1, 4), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(2, 4), int(1, 4), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(1, 8), int(1, 8), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(int(2, 8), int(1, 8), __FILE__, __LINE__, test_suite_great_than)
   
-  ! -> real4
-  call test_case_create('real4', test_suite_great_than)
-  call assert_great_than(1.0, 1.0, suite=test_suite_great_than)
-  call assert_great_than(2.0, 1.0, suite=test_suite_great_than)
+  call assert_great_than([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [int(0, 1), int(2, 1), int(0, 1), int(0, 1)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [int(0, 1), int(0, 1), int(0, 1), int(0, 1)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [int(0, 2), int(2, 2), int(0, 2), int(0, 2)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [int(0, 2), int(0, 2), int(0, 2), int(0, 2)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [int(0, 4), int(2, 4), int(0, 4), int(0, 4)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [int(0, 4), int(0, 4), int(0, 4), int(0, 4)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [int(0, 8), int(2, 8), int(0, 8), int(0, 8)], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [int(0, 8), int(0, 8), int(0, 8), int(0, 8)], __FILE__, __LINE__, test_suite_great_than)
   
-  ! -> real8
-  call test_case_create('real8', test_suite_great_than)
+  call assert_great_than(reshape([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [2, 2]), &
+      & reshape([int(0, 1), int(2, 1), int(0, 1), int(0, 1)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([int(1, 1), int(1, 1), int(1, 1), int(1, 1)], [2, 2]), &
+      & reshape([int(0, 1), int(0, 1), int(0, 1), int(0, 1)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  
+  call assert_great_than(reshape([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [2, 2]), &
+      & reshape([int(0, 2), int(2, 2), int(0, 2), int(0, 2)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([int(1, 2), int(1, 2), int(1, 2), int(1, 2)], [2, 2]), &
+      & reshape([int(0, 2), int(0, 2), int(0, 2), int(0, 2)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  
+  call assert_great_than(reshape([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [2, 2]), &
+      & reshape([int(0, 4), int(2, 4), int(0, 4), int(0, 4)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([int(1, 4), int(1, 4), int(1, 4), int(1, 4)], [2, 2]), &
+      & reshape([int(0, 4), int(0, 4), int(0, 4), int(0, 4)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  
+  call assert_great_than(reshape([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [2, 2]), &
+      & reshape([int(0, 8), int(2, 8), int(0, 8), int(0, 8)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([int(1, 8), int(1, 8), int(1, 8), int(1, 8)], [2, 2]), &
+      & reshape([int(0, 8), int(0, 8), int(0, 8), int(0, 8)], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  
+  ! -> real (real4, real8)
+  call test_case_create('real', test_suite_great_than)
+  call assert_great_than(1.0, 1.0, __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(2.0, 1.0, __FILE__, __LINE__, test_suite_great_than)
   call assert_great_than(1.0D0, 1.0D0, __FILE__, __LINE__, test_suite_great_than)
   call assert_great_than(2.0D0, 1.0D0, __FILE__, __LINE__, test_suite_great_than)
   
-  ! report
+  call assert_great_than([1.0, 1.0], [0.0, 2.0], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([1.0, 1.0], [0.0, 0.0], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([1.0D0, 1.0D0], [0.0D0, 2.0D0], __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than([1.0D0, 1.0D0], [0.0D0, 0.0D0], __FILE__, __LINE__, test_suite_great_than)
+  
+  call assert_great_than(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([0.0, 2.0, 0.0, 0.0], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([1.0, 1.0, 1.0, 1.0], [2, 2]), reshape([0.0, 0.0, 0.0, 0.0], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([0.0D0, 2.0D0, 0.0D0, 0.0D0], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+  call assert_great_than(reshape([1.0D0, 1.0D0, 1.0D0, 1.0D0], [2, 2]), reshape([0.0D0, 0.0D0, 0.0D0, 0.0D0], [2, 2]), __FILE__, __LINE__, test_suite_great_than)
+
   call test_suite_report(test_suite_great_than)
   call test_case_final(test_suite_great_than)
 
